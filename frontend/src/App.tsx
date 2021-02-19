5a6dc1b395064b55df8e5f255e9ef9798e37ec2b
user.castellana <r_aquel@hotmail.com>
2021-02-19 13:44:31 +0100

    taks added in a list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,43 @@
 import React, { Fragment, useState } from 'react'
 
 /* import './App.css'; */
+type FormElement = React.FormEvent<HTMLFormElement>
+
+interface ITask { /* especificamos qué propiedades tendrán mis tareas */
+    name: string;
+    done: boolean;
+}
 
 function App(): JSX.Element { //": JSX.Element" cuando sea creado el elemento App, será retornado como jsx
-  const [newTask, setNewTask] = useState('')
+    const [newTask, setNewTask] = useState<string>('')
+    const [tasks, setTasks] = useState<ITask[]>([]) /* useState será una lista de objetos [{}, {}] */
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault  /* para que no se refresque la página cada vez que guardemos una tarea */
-      console.log('newTask')
+    const handleSubmit = (e: FormElement) => {
+      e.preventDefault();
+      /* console.log(newTask) */
+      addTask(newTask)
+      /* console.log(tasks) */
+      setNewTask(''); /* una vez que hayamos agregado una tarea, desaparece el texto del inputfeld */
+   }
+  
+    const addTask = (name: string) => {
+       const newTasks: ITask[] = [...tasks, {name: name, done: false}] /* puedo especificar aquí también que const newTasks es de tipo ": ITask[]" */
+       setTasks(newTasks)
     }
-  return (
-      <Fragment>
-        <form onSubmit={handleSubmit}>
-           {/*<input type="text" onChange={e => console.log(e.target.value)}/> */} {/*cada vez que el usuario tipee algo, se actualizará el state */}
-          {/* onChange es una función que tiene como parámetro un evento, pero yo sólo quiero el valor de lo que está tipeando */}
-          {/*Pero yo no quiero verlo en consola, sino que quiero pasarle esos datos el newTask. Voy a establecer un State de esa tarea que he creado: e => setNewTask: */}
-          {/* <input type="text" onChange={e => setNewTask()}/> */}
-          {/*Ahora establecemos un valor a "newTask". Le damos el valor e.target.value: */}
-          <input type="text" onChange={e => setNewTask(e.target.value)}/>
-          <button>Save</button>
-        </form>
-      </Fragment>
-  );
+    return (
+        <Fragment>
+            <form onSubmit={handleSubmit}>
+            {/*Voy a establecer un State de esa tarea que he creado: e => setNewTask.Utilizando la función "setNewTask" podremos asignar un valor a "newTask", en este caso queremos asignarle el valor e.target.value: */}
+                <input type="text" onChange={e => setNewTask(e.target.value)} value={newTask}/>
+                <button>Save</button>
+            </form>
+
+            {tasks.map((t: ITask, i: number) => {
+                return <h2 key={i}>{t.name}</h2>
+            })
+            }
+        </Fragment>
+    );
 }
 
 export default App;
